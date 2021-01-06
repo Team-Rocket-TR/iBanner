e2731e338864f166583c0e8c78cd736665e9fff6
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-01-06 22:58:27 +0900

    Git pull upstream main을 위한 코드 commit

--- src/BannerContainer.jsx
+++ src/BannerContainer.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+
 import BannerPreview from './BannerPreview';
 import ColorpickerIcon from './ColorpickerIcon';
 
 const BannerContainer = () => {
   const [color, setColor] = useState('black');
+
   const handleClick = (selectedColor) => {
     setColor(selectedColor);
   };
