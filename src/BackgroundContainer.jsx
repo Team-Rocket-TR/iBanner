21ee9e0729627fe9f4290716eede324e4263acc2
raeyoung <raeyoung233@gmail.com>
2021-01-18 22:26:26 +0900

    Change banner Background color
    Change the color of the banner background using react-color
    Use react-color Blockpicker

--- src/BackgroundContainer.jsx
+++ src/BackgroundContainer.jsx
@@ -11,12 +11,13 @@ import ColorpickerIcon from './ColorpickerIcon';
 
 const BackgroundContainer = () => {
   const dispatch = useDispatch();
+  const backgroundColor = useSelector((state) => state.color);
   const width = useSelector((state) => state.width);
   const height = useSelector((state) => state.height);
 
   // Change banner background color
-  const handleClick = (selectedColor) => {
-    dispatch(setBackgroundColor(selectedColor));
+  const handleChangeComplete = (color) => {
+    dispatch(setBackgroundColor(color.hex));
   };
 
   // Change banner width
@@ -39,7 +40,7 @@ const BackgroundContainer = () => {
     <>
       <input type="text" name="width" value={width} maxLength="3" placeholder="Banner Width" onChange={handleWidthChange} />
       <input type="text" name="height" value={height} maxLength="3" placeholder="Banner Height" onChange={handleHeightChange} />
-      <ColorpickerIcon onClick={handleClick} />
+      <ColorpickerIcon handleChangeComplete={handleChangeComplete} color={backgroundColor} />
     </>
   );
 };
