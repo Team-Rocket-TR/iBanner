212c947d0f0928007abf7179977894aafc184e5e
raeyoung <raeyoung233@gmail.com>
2021-01-18 22:29:13 +0900

    Delete Annotation

--- src/ColorpickerIcon.jsx
+++ src/ColorpickerIcon.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-// import styled from 'styled-components';
 import { BlockPicker } from 'react-color';
-
-// const BannerPaintingBtn = styled.div`
-//   display: -webkit-flex;
-//   justify-content: center;
-//   padding-top: 1rem;
-// `;
 
 const ColorpickerIcon = ({ color, handleChangeComplete }) => (
   <BlockPicker
@@ -14,12 +7,4 @@ const ColorpickerIcon = ({ color, handleChangeComplete }) => (
     onChangeComplete={handleChangeComplete}
   />
 );
-
-// <BannerPaintingBtn>
-//   <button type="button" onClick={() => onClick('pink')}>pink</button>
-//   <button type="button" onClick={() => onClick('blue')}>blue</button>
-//   <button type="button" onClick={() => onClick('red')}>red</button>
-//   <button type="button" onClick={() => onClick('black')}>black</button>
-// </BannerPaintingBtn>
-
 export default ColorpickerIcon;
