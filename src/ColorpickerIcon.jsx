945d5b34b839ed74ae9873a35581a25c0dd229a7
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-01-25 21:45:19 +0900

    Change style about Background Color Picker('react-color')
    - <BlockPicker> -> <CirclePicker> + < MaterialPicker>
    
    *TODO: UI 조절 필요!

--- src/ColorpickerIcon.jsx
+++ src/ColorpickerIcon.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { BlockPicker } from 'react-color';
+import { CirclePicker, MaterialPicker } from 'react-color';
 
-const ColorpickerIcon = ({ color, handleChangeComplete }) => (
-  <BlockPicker
-    color={color}
-    onChangeComplete={handleChangeComplete}
-  />
-);
+const ColorpickerIcon = ({ color, handleChangeComplete }) => {
+  const colors = [
+    '#f44336', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#009688',
+    '#4caf50', '#8bc34a', '#ffee4b', '#ff9800', '#ffffff', '#000000',
+  ];
+
+  return (
+    <>
+      <CirclePicker
+        colors={colors}
+        onChangeComplete={handleChangeComplete}
+      />
+      <MaterialPicker
+        color={color}
+        onChangeComplete={handleChangeComplete}
+      />
+    </>
+  );
+};
 export default ColorpickerIcon;
