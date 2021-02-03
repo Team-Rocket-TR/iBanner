9ae790a936eb25068aed9b95977b472a01824e85
EHOTO <ktaewoong46@gmail.com>
2021-02-03 10:51:22 +0900

    Application Deployment Preferences
    - Add new properties in 'webpack.config.js'
    - Add new script in 'package.json'
    
    * If you want to build application, Run 'npm run build' as a command!

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
-const path = require('path');
+const path = require('path'); // core nodejs 모듈 중 하나, 파일 경로 설정할 때 사용
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(`${__dirname}/build`),
+    filename: 'bundle.js',
+  },
   module: {
     rules: [
       {
@@ -8,8 +14,15 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './index.html' }), // 생성한 템플릿 파일
+  ],
   resolve: {
     modules: [
       'node_modules',
