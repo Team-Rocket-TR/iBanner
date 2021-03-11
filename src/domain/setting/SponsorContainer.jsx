c8e43296a3e76c60ac5e9d24931bd991ab05bdb9
raeyoung <raeyoung233@gmail.com>
2021-03-11 16:00:56 +0900

    Move from SponsorContainer to Sponsor Component

--- src/domain/setting/SponsorContainer.jsx
+++ src/domain/setting/SponsorContainer.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-function SponsorContainer() {
+import Sponsor from './components/sponsor/Sponsor';
+
+const SponsorContainer = () => {
     return (
-        <div>
-            <p id='sponsorText'>카카오페이로 후원하기</p>
-            <img id='sponsor' src='src/public/image/kakaopay_QRcode.png' alt='kakaopay'/>
-        </div>
+        <Sponsor />
     )
 }
 
---
+++ src/domain/setting/components/sponsor/Sponsor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Sponsor = () => {
+    return (
+        <div>
+            <p id='sponsorText'>카카오페이로 후원하기</p>
+            <img id='sponsor' src='src/public/image/kakaopay_QRcode.png' alt='kakaopay' />
+        </div>
+    )
+}
+
+export default Sponsor
