bcb466decdecb77795b4ba18dc62fdbacb5e6df5
raeyoung <raeyoung233@gmail.com>
2021-03-16 15:40:01 +0900

    마지막 QRcode url 수정

--- src/domain/setting/components/sponsor/Sponsor.jsx
+++ src/domain/setting/components/sponsor/Sponsor.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import kakaopayQRcode from '/image/kakaopay_QRcode.png';
+//import kakaopayQRcode from '/image/kakaopay_QRcode.png';
 
 const Sponsor = () => {
     return (
         <div>
             <p id='sponsorText'>카카오페이로 후원하기</p>
-            <img id='sponsor' src={kakaopayQRcode} alt='kakaopay' />
+            <img id='sponsor' src='/image/kakaopay_QRcode.png' alt='kakaopay' />
         </div>
     )
 }
