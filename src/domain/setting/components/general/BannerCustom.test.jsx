7214fd7308fd83f2e106164769f4c4e262bf0c01
raeyoung <raeyoung233@gmail.com>
2021-08-01 16:47:28 +0900

    BannerCustom test 코드 작성중 (재커밋)

---
+++ src/domain/setting/components/general/BannerCustom.test.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import BannerCustom from './BannerCustom';
+
+describe('BannerCustom', () => {
+  const inputs = [
+    {
+      name: 'width',
+      placeholder: 'Banner Width',
+    },
+    {
+      name: 'height',
+      placeholder: 'Banner Height',
+    },
+  ];
+  const handleChange = jest.fn();
+
+  function renderInput({
+    width = 500,
+    height = 500,
+  }) {
+    return render((
+      <BannerCustom width={width} height={height} onChange={handleChange} />
+    ));
+  }
+
+  it('renders two inputs', () => {
+    const { getByPlaceholderText } = renderInput();
+
+    inputs.map({placeholder} => {
+      expect(getByPlaceholderText(placeholder)).not.toBeNull();
+    });
+  });
+
+});
