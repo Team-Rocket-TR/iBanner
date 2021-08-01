6b8f6edb9e1964862558b0ab108bcc3ad7416911
raeyoung <raeyoung233@gmail.com>
2021-08-01 19:11:33 +0900

    BannerCustomSize 컴포넌트 테스트 코드 작성완료

---
+++ src/domain/setting/components/general/BannerCustomSize.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import BannerCustomSize from './BannerCustomSize';
+
+describe('BannerCustomSize component', () => {
+  const handleChange = jest.fn();
+
+  function renderCustomSize({
+    name = 'width',
+    value = '500',
+    placeholder = 'Banner Width',
+  } = {}) {
+    return render((
+      <BannerCustomSize
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={handleChange}
+      />
+    ));
+  }
+
+  it('width palceHolder value', () => {
+    const { getByPlaceholderText } = renderCustomSize();
+
+    expect(getByPlaceholderText(/Banner Width/)).not.toBeNull();
+  });
+
+  context('banner width text', () => {
+    it('renders width text', () => {
+      const { container } = renderCustomSize();
+
+      expect(container).not.toBeNull();
+    });
+  });
+
+  it('width size change event', () => {
+    const { getByPlaceholderText } = renderCustomSize();
+    const target = getByPlaceholderText('Banner Width');
+
+    expect(target).not.toBeNull();
+
+    fireEvent.change(target, { target: { value: '300' } });
+
+    expect(handleChange).toBeCalledTimes(1);
+  });
+});
