e0425858fdb98c3d98811248ba6f26f64d6a454f
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-08-06 11:37:25 +0900

    `index.js`에 `TextInputContainer` 추가

--- src/domain/setting/containers/index.jsx
+++ src/domain/setting/containers/index.jsx
@@ -2,10 +2,12 @@ import GeneralContainer from './GeneralContainer';
 import BackgroundContainer from './BackgroundContainer';
 import ContentContainer from './ContentContainer';
 import SponsorContainer from './SponsorContainer';
+import TextInputContainer from './TextInputContainer';
 
 export {
   GeneralContainer,
   BackgroundContainer,
   ContentContainer,
   SponsorContainer,
+  TextInputContainer,
 };
