e5ff8ef7100f9b5a8067787b715a251838e0cc81
TaeWoongKim2 <ktaewoong46@gmail.com>
2021-08-06 16:28:31 +0900

    가독성을 위한 Dashed Line 적용
    바둑선이 너무 선명하지 않도록 `Dashed Line` 프로퍼티 적용했습니다.

--- src/domain/preview/components/helper-layer/GridLine.jsx
+++ src/domain/preview/components/helper-layer/GridLine.jsx
@@ -6,12 +6,14 @@ export default function GridLine({
   points,
   stroke = '#ddd',
   strokeWidth = 1,
+  dash = [5, 2],
 }) {
   return (
     <Line
       points={points}
       stroke={stroke}
       strokeWidth={strokeWidth}
+      dash={dash}
     />
   );
 }
